22caa010fb221c7a8f7f9fd07c741a8030eee4a7
Hannah Donovan <34870726+hannahcdonovan@users.noreply.github.com>
2019-11-20 22:43:31 -0500

    deleted main and updated comments for city class

--- City.java
+++ City.java
@@ -1,30 +1,58 @@
 public class City {
 
+    /**
+     * Integer identification instance of a City object.
+     */
     private int id;
 
+    /**
+     * The xCoordinate of the City.
+     */
     private double xCoord;
 
+    /**
+     * The yCoordinate of the City.
+     */
     private double yCoord;
 
+    /**
+     * Constructor for the City object.
+     */
     public City(int id, double xCoord, double yCoord) {
         this.id = id;
         this.xCoord = xCoord;
         this.yCoord = yCoord;
     }
 
+    /**
+     * toString method.
+     * @return String representation of the City.
+     */
     public String toString() {
         String cityRep = "City: " + this.id + "Coordinates: " + "[ " + this.xCoord + "," + this.yCoord + "]";
         return cityRep;
     }
 
+    /**
+     * xCoord getter.
+     * @return the xCoord of the City object.
+     */
     public double getXCoord() {
         return this.xCoord;
     }
 
+    /**
+     * yCoord getter.
+     * @return the xCoord of the City object.
+     */
     public double getYCoord() {
         return this.yCoord;
     }
 
+    /**
+     * id getter.
+     * @return the yCoord of the City object.
+     */
     public int getCityId() {
         return this.id;
     }
